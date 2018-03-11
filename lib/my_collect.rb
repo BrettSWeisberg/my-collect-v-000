a0b8029fbd89ce5e0d3b4265b06d5836a2dff6d9
Brett Weisberg <weisberg.brett@gmail.com>
2018-03-11 18:38:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,3 +1,12 @@
 
 def my_collect(array)
+  counter = 1
+  collection = []
+  while counter < array.length
+    collection << yield(array[i])
+    i += 1
 end
+collection
+end
+
+{|value| value.split(" ").first}
